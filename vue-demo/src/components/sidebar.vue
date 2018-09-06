10e81ab2ed396b19b4e7bf46cf6dd556bb3e3670
YakumoAoi <416910533@qq.com>
2018-09-06 22:28:52 +0900

    iniatialize

---
+++ vue-demo/src/components/sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tryTitle">
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'firstTry',
+    data(){
+        return{
+            message:'首次尝试'
+        }
+    }
+}
+</script>
+
+<style>
+    .tryTitle{
+        height:200px;
+        color:#fff;
+        background-color: rgb(255, 157, 0);
+        text-align:center;
+    }
+    .tryTitle h1{
+        font-size:60px;
+    }
+</style>
+
+
